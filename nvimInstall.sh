9c54bd97e92403a7ff108f06fe4a34d3cf31fbf3
aikenhong <h.aiken.970@gmail.com>
2023-09-07 09:16:59 +0800

    <code> Update NvimInstall Script,

--- nvimInstall.sh
+++ nvimInstall.sh
@@ -7,54 +7,85 @@ source ./FunctionList.sh
 proxy="172.30.240.1:8890"
 
 # 0.install miniconda for python
-echo "------------------install miniconda for neovim"
-wget -c https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-latest-Linux-x86_64.sh
-chmod 777 Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh
-rm Miniconda3-latest-Linux-x86_64.sh
+echo "***************install miniconda for neovim"
+
+if [[ ! $(conda -V) ]];then 
+  wget -c -O miniconda_install.sh https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-latest-Linux-x86_64.sh
+  chmod 777 miniconda_install.sh
+  bash miniconda_install.sh
+  exec_cmd_status "install conda process"
+  rm -v miniconda_install.sh
+  source ~/.zshrc
+else
+  echo "conda had installed, pass"
+fi
 
 # 1.install neovim (using unstable for diff sys)
-echo "------------------install neovim"
-sudo apt-get install software-propertier-common
-sudo add-apt-repository ppa:neovim-ppa/unstable
-sudo apt-get update
-sudo apt-get install neovim
+echo "***************install neovim"
+grep -qr neovim /etc/apt/sources.list.d/* 
+if [[ $? -ne 0 ]];then
+  sudo apt-get install software-propertier-common
+  sudo add-apt-repository ppa:neovim-ppa/unstable
+  sudo apt-get update
+  sudo apt-get install neovim
+else
+  sudo apt-get install neovim
+fi
+exec_cmd_status "install neovim process"
 
-nvim_version=$(nvim -v | grep dev)
-echo "neovim's version is $nvim_version"
+nvim_version=$(nvim -v | grep -i nvim)
+echo "nvim ver.is : $nvim_version"
 
 # 2. install dependency of neovim
-echo "-------------------install dependency"
-sudo apt-get install python-pip python3-dev
-sudo apt-get install python3
-sudo pip3 install pynvim
-sudo pip3 install neovim
+echo "***************install dependency"
+sudo pip install pynvim
+exec_cmd_status "pynvim install"
+
+sudo pip install neovim
+exec_cmd_status "pip install neovim"
 
 # 3. install lazygit
-echo "--------------------install lazygit"
-LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz" --proxy $proxy
-tar xf lazygit.tar.gz lazygit
-sudo install lazygit /usr/local/bin
-rm lazygit.tar.gz
+echo "***************install lazygit"
+lazygit -v
+if [[ $? -ne 0 ]];then 
+  LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+  curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz" --proxy $proxy
+  tar xf lazygit.tar.gz lazygit
+  sudo install lazygit /usr/local/bin
+  exec_cmd_status "install lazygit"
+  rm -v lazygit.tar.gz
+  rm -v lazygit
+  echo "lazygit version $LAZYGIT_VERSION had installed"
+else
+  echo "lazygit had installed, pass"
+fi
 
 # 4. install ripgrep, fd-find
-echo "--------------------install ripgrep fd-find"
+echo "***************install ripgrep fd-find"
 sudo apt-get install ripgrep
+exec_cmd_status "install ripgrep"
+
 sudo apt-get install fd-find
+exec_cmd_status "install fd-find"
 
 # 5. install nvm to install nodejs.(need restart)
-echo "---------------------install nvm and nodejs, npm"
-curl --proxy $proxy -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-source ~/.bashrc
-echo "you need restart to reboot nvm, then we use nvm to install node"
+echo "***************install nvm and nodejs, npm"
+nvm -v 
+if [[ $? -ne 0 ]]; then
+  curl --proxy $proxy -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+  exec_cmd_status "install nvm"
+  source ~/.bashrc
+  echo "you need restart to reboot nvm, then we use nvm to install node"
+  # cpy nvm 2 .zshrc
+  cat ~/.bashrc | grep -i nvm >>~/.zshrc # init zsh
+  # becus we install nvm in bash, we move config here.
+  echo "export NVM_DIR=\"$HOME/.nvm\"" >>~/.zshrc
+  echo "[ -s \"$NVM_DIR/nvm.sh\" ] && \. \"$NVM_DIR/nvm.sh\"" >>~/.zshrc                   # This loads nvm
+  echo "[ -s \"$NVM_DIR/bash_completion\" ] && \. \\"$NVM_DIR/bash_completion\" >>~/.zshrc # This loads nvm bash_completion" >> ~/.zshrc
+else
+  echo "nvm had installed"
+fi
 
-# cpy nvm 2 .zshrc
-cat ~/.bashrc | grep -i nvm >>~/.zshrc # init zsh
-# becus we install nvm in bash, we move config here.
-echo "export NVM_DIR=\"$HOME/.nvm\"" >>~/.zshrc
-echo "[ -s \"$NVM_DIR/nvm.sh\" ] && \. \"$NVM_DIR/nvm.sh\"" >>~/.zshrc                   # This loads nvm
-echo "[ -s \"$NVM_DIR/bash_completion\" ] && \. \\"$NVM_DIR/bash_completion\" >>~/.zshrc # This loads nvm bash_completion" >> ~/.zshrc
 
 # manager specific npm version
 nvm list-remote
@@ -70,5 +101,5 @@ ruby -v
 gem environment
 gem install neovim
 
-# 7. start nvim
-nvim
+# 7. install neovim 
+echo "Neovim Install Success. U can Start And Try it"
